711f9384c9a9a330df2563e96b739546097aedc4
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2023-09-25 23:37:25 -0300

    primera_entrega

--- entrega.js
+++ entrega.js
@@ -47,7 +47,7 @@ class ProductManager {
   getProductById(id) {
   const product = this.products.find((product) => product.id === id);
   if (!product) {
-    console.error("Producto no encontrado");
+    console.error("Not Found");
   }
   return product;
 }
